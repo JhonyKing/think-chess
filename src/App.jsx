194b99efb2dba6a79dfe08c18b9f02386af793ea
Jhonnatan Vazquez <jhonykingmusic@hotmail.com>
2024-11-22 16:12:53 -0600

    ending the first steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
-      <div>Hello World!</div>;
+      <StyledApp>
+        <Heading as="h1">Think chess</Heading>
+        <Heading as="h2">The best in the world!</Heading>
+        <div>Hello World!</div>
+        <Button>Hey</Button>
+        <Input />
+      </StyledApp>
     </>
   );
 }
